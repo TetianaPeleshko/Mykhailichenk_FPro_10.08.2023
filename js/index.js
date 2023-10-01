57b15fbd3cfc41e55ce45272bf0e9cbac6759f4b
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-01 17:24:49 +0300

    HW_3_MathOperations

--- js/index.js
+++ js/index.js
@@ -1,3 +1,16 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+let num1 = prompt('Enter the first number', '5');
+let num2 = prompt('Enter the second number', '5');
+
+if (Number(num1) && Number(num2)) {
+  let sum = +num1 + +num2;
+  let sub = num1 - num2;
+  let mult = num1 * num2;
+  let div = num1 / num2;
+  alert(`The user entered ${num1} and ${num2}:
+  ${num1} + ${num2} = ${sum}
+  ${num1} - ${num2} = ${sub};
+  ${num1} * ${num2} = ${mult};
+  ${num1} / ${num2} = ${div}`);
+} else {
+  alert('Please enter the numbers');
+}
