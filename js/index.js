fad415f7a12dfa1485669109fca20d420143bb15
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-01 17:37:37 +0300

    HW_3_MathOperations_WithZero

--- js/index.js
+++ js/index.js
@@ -1,7 +1,10 @@
 let num1 = prompt('Enter the first number', '5');
 let num2 = prompt('Enter the second number', '5');
 
-if (Number(num1) && Number(num2)) {
+if (
+  (Number(num1) || Number(num1) == 0) &&
+  (Number(num2) || Number(num2) == 0)
+) {
   let sum = +num1 + +num2;
   let sub = num1 - num2;
   let mult = num1 * num2;
