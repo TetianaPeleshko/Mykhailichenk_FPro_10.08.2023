f9f5546708fdb3b94b0b6755193930aee6f64939
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-01 20:08:09 +0300

    HW_4_Calculator

--- js/index.js
+++ js/index.js
@@ -1,3 +1,37 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+let calc = prompt('What do you whant to do: add, sub, mult, div?');
+let num1 = prompt('Enter a first number:', '2');
+let num2 = prompt('Enter a second number:', '2');
+
+let result;
+let oper;
+
+if ((Number(num1) || num1 == '0') && (Number(num2) || num2 == '0')) {
+  switch (calc) {
+    case 'add':
+      result = +num1 + +num2;
+      oper = '+';
+      break;
+    case 'sub':
+      result = num1 - num2;
+      oper = '-';
+      break;
+    case 'mult':
+      result = num1 * num2;
+      oper = '*';
+      break;
+    case 'div':
+      result = num1 / num2;
+      oper = '/';
+      break;
+    default:
+      alert('Wrong operation input');
+  }
+
+  if (Number(result)) {
+    result = +result.toFixed(2);
+    let showResult = `${num1} ${oper} ${num2} = ${result}`;
+    alert(showResult);
+  }
+} else {
+  alert('Wrong number input');
+}
