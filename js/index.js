a257193e4e51f12cfbe8ea85447386c65a6748f3
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-02 14:53:41 +0300

    HW_5_UserPortrait

--- js/index.js
+++ js/index.js
@@ -1,3 +1,63 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+let yearBirth = prompt('Який твій рік народження', '1982');
+let age;
+
+if (yearBirth === null || yearBirth === '') {
+  alert('Шкода, що Ви не захотіли ввести свій рік народження');
+} else {
+  let numYearBirth = Number(yearBirth); // liquidation undefind in alert
+  if (!isNaN(numYearBirth)) {
+    let currentYear = new Date().getFullYear();
+    age = `Твій вік: ${currentYear - yearBirth}`;
+  } else {
+    alert('The date is not correct. Please enter a number');
+  }
+}
+age = age || '';
+
+let cityLive = prompt('В якому місті ти живеш?', 'Харків');
+let resultCity;
+
+if (cityLive === null || cityLive.trim() === '' || !isNaN(cityLive)) {
+  alert('Шкода, що Ви не захотіли ввести свое місто');
+} else {
+  switch (cityLive) {
+    case 'Київ':
+      resultCity = `Ти живеш у столиці України`;
+      break;
+    case 'Вашингтон':
+      resultCity = `Ти живеш у столиці США`;
+      break;
+    case 'Лондон':
+      resultCity = `Ти живеш у столиці Великобританії`;
+      break;
+    default:
+      resultCity = `Ти живеш у місті ${cityLive}`;
+  }
+}
+
+// Set a default message if resultCity is undefined
+resultCity = resultCity || '';
+
+let sport = prompt('Який твій улюбленний вид спорту?', 'Футбол');
+let star;
+
+if (sport === null || sport.trim() === '' || !isNaN(cityLive)) {
+  alert('Шкода, що Ви не захотіли ввести свій улюблений вид спорту');
+} else {
+  switch (sport) {
+    case 'Теніс':
+      star = 'Круто! Хочеш стати як Андрій Медведев';
+      break;
+    case 'Футбол':
+      star = 'Круто! Хочеш стати як Андрій Шевченко?';
+      break;
+    case 'Дзюдо':
+      star = 'Круто! Хочеш стати як Дар`я Білодід?';
+      break;
+  }
+}
+star = star || '';
+
+alert(`${age}
+${resultCity}
+${star}`);
