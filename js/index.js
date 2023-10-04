7b8cfe44c5b16ac66ce80e185ed3b3a5bf4361d6
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-04 22:06:30 +0300

    HW_8_Arrays

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+let arrLength = +prompt('Enter the length of the array:', '7');
+let arr = [];
+
+if (isNaN(arrLength) || arrLength <= 0) {
+  alert('Please, enter the correct number!');
+} else {
+  for (let i = 0; i <= arrLength; i++) {
+    let elements = +prompt('Enter the elements of the array', '2');
+    arr.push(elements);
+  }
+}
+console.log(arr);
+
+arr.sort((a, b) => a - b);
+console.log(`Масив за зростанням: ${arr}`);
+
+arr.splice(1, 3);
+console.log(`Видалити елементи з масиву з 2 по 4 (включно!); ${arr}`);
