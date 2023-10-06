e20d326938fe0eb256900835da2a2f25b8c369f2
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-06 18:33:25 +0300

    HW_11_FunctionRemoveElement

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+// Реалізуйте функцію removeElement(array, item), щоб видалити елемент item з масиву array.
+
+function removeElement(array, item) {
+  return array.filter((element) => element !== item);
+}
+const array = [1, 2, 3, 4, 5, 6, 7];
+let newArray = removeElement(array, 5);
+console.log(newArray);
