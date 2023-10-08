79264438121b33c000d3df5116918ac0ca3c91fc
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-09 01:17:41 +0300

    HW_13_RecursiveExponentiation

--- js/index.js
+++ js/index.js
@@ -1,3 +1,26 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+// Recursion
+// function func(number) {
+//   if (number === 0) {
+//     return 1;
+//   } else {
+//     return number * func(number - 1);
+//   }
+// }
+
+// console.log(func(5));
+
+// exponentiation
+const num = 3;
+const degree = 4;
+
+function pow(num, degree) {
+  if (degree === 0) {
+    return 1;
+  } else if (num === 0) {
+    return 0;
+  } else {
+    return num * pow(num, degree - 1);
+  }
+}
+
+console.log(`${num} в ступені ${degree} дорівнює ${pow(num, degree)}`);
