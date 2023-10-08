d9a2f4f8ff7af78dd38cf49ccd130be3214dc084
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-09 00:35:07 +0300

    HW_12_Function_generateKey

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+
+function generateKey(length, characters) {
+  let key = '';
+  for (let i = 0; i < length; i++) {
+    const randomIndex = Math.round(Math.random() * (characters.length - 1)); // або так const randomIndex = Math.floor(Math.random() * (characters.length));
+    // console.log(randomIndex);
+    key += characters[randomIndex];
+  }
+  return key;
+}
+
+const key = generateKey(16, characters);
+console.log(key); // eg599gb60q926j8i
