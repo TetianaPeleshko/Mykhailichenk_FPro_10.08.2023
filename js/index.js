5d8e2f4065c4bcbc92f8f64fd904735b41bb99dc
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-12 17:29:04 +0300

    HW_15_functionWithClosures

--- js/index.js
+++ js/index.js
@@ -1,3 +1,17 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+// Написати функцію, яка приймає 1 параметр. з тим, що передали перший раз і т. д. Все це із замиканнями, наприклад:
+
+// sum(3) = 3
+// sum(5) = 8
+// sum(20) = 28
+
+function sumParametrs() {
+  let result = 0;
+  return function add(num) {
+    result += num;
+    return result;
+  };
+}
+let sum = sumParametrs();
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(20));
