5261aa7ac43f5bad4b46fb2bc66ec2cbc614c121
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-15 09:02:58 +0300

    HW_17_divGhost

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+const textField = document.getElementById('input-text');
+const divGhost = document.getElementById('div-ghost');
+
+function handleFocus() {
+  divGhost.style.display = 'block';
+}
+function handleBlur() {
+  divGhost.style.display = 'none';
+}
+textField.addEventListener('focus', handleFocus);
+textField.addEventListener('blur', handleBlur);
