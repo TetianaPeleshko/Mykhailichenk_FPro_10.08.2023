ece4e71c0eda55ea1b00d7d77b61459c9c9f51bd
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-16 14:55:53 +0300

    HW_HW_18_randomImageOutput

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+// Task: У папці images є зображення 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5 .jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg Вивести зображення з цієї папки, отримане випадковим чином (Math.random)
+
+const imageName = [
+  '1.jpg',
+  '2.jpg',
+  '3.jpg',
+  '4.jpg',
+  '5.jpg',
+  '6.jpg',
+  '7.jpg',
+  '8.jpg',
+  '9.jpg',
+];
+
+let randomImage = imageName[Math.floor(Math.random() * imageName.length)];
+// console.log(randomImage);
+
+const img = document.createElement('img');
+img.src = `images/${randomImage}`;
+
+document.getElementById('random-image').appendChild(img);
