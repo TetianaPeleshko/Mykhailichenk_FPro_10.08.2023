37beb335f6c28c6be0a4c36f6901f19278cf0119
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-18 23:32:02 +0300

    HW_21_RegistratioForm

--- js/index.js
+++ js/index.js
@@ -131,3 +131,70 @@ function buyProduct(product) {
   productsContainer.innerHTML = '';
   productsInfoContainer.innerHTML = '';
 }
+
+//
+//FORM
+document.getElementById('saveButton').addEventListener('click', function () {
+  if (validateForm()) {
+    submitForm();
+  } else {
+    alert('Please fill in all required fields');
+  }
+});
+
+function validateForm() {
+  const requiredFields = document.querySelectorAll('[required]');
+  let isValid = true;
+
+  requiredFields.forEach((field) => {
+    if (!field.value.trim()) {
+      isValid = false;
+    }
+  });
+  return isValid;
+}
+
+function submitForm() {
+  const formData = {
+    firstName: document.getElementById('firstName').value,
+    lastName: document.getElementById('lastName').value,
+    birthDate: document.getElementById('birthDate').value,
+    gender: document.querySelector('input[name="gender"]:checked').value,
+    city: document.getElementById('city').value,
+    address: document.getElementById('address').value,
+    languages: getSelectedLanguages(),
+  };
+  displayUserData(formData); // вивід даних у таблицю
+}
+
+function getSelectedLanguages() {
+  const checkboxes = document.querySelectorAll(
+    'input[name="language"]:checked'
+  );
+  const selectedLanguages = [];
+
+  checkboxes.forEach((checkbox) => {
+    selectedLanguages.push(checkbox.value);
+  });
+  return selectedLanguages;
+}
+
+function displayUserData(data) {
+  const userDataDiv = document.getElementById('userData');
+  userDataDiv.innerHTML = ''; // Зачистити попередню дату
+
+  const table = document.createElement('table');
+  const headerRow = table.insertRow(0);
+  const dataRow = table.insertRow(1);
+
+  for (const key in data) {
+    const headerCell = headerRow.insertCell(-1); // вставляє нову комірку у вказаному індексі,  в кінець рядка
+    const dataCell = dataRow.insertCell(-1); //
+
+    headerCell.textContent = key;
+    dataCell.textContent = Array.isArray(data[key])
+      ? data[key].join(', ')
+      : data[key];
+  }
+  userDataDiv.appendChild(table);
+}
