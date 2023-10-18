3ac36ff5dc7ab5a56bd6841b7a711f4c1420d16d
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-18 19:15:03 +0300

    HW_20_basicSlider

--- js/index.js
+++ js/index.js
@@ -1,21 +1,44 @@
-// Task: У папці images є зображення 1.jpg, 2.jpg, 3.jpg, 4.jpg, 5 .jpg, 6.jpg, 7.jpg, 8.jpg, 9.jpg Вивести зображення з цієї папки, отримане випадковим чином (Math.random)
+// Пишемо свій слайдер зображень.
 
-const imageName = [
-  '1.jpg',
-  '2.jpg',
-  '3.jpg',
-  '4.jpg',
-  '5.jpg',
-  '6.jpg',
-  '7.jpg',
-  '8.jpg',
-  '9.jpg',
-];
+// Відображаємо зображення та кнопки Next, Prev з боків від зображення.
+// При кліку на Next - показуємо наступне зображення.
+// При кліку на Prev - попереднє .
+// При досягненні останнього зображення - ховати кнопку Next. Аналогічно з першим зображенням і кнопкою Prev
 
-let randomImage = imageName[Math.floor(Math.random() * imageName.length)];
-// console.log(randomImage);
+let currentIndex = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
 
-const img = document.createElement('img');
-img.src = `images/${randomImage}`;
+function showSlide(index) {
+  currentIndex = Math.min(Math.max(index, 0), totalSlides - 1); // Забезпечуємо індекс в межах допустимих значень
+  // Math.max(index, 0): вибирає більше з двох значень - index та 0. Якщо index менше за 0, то вибирається 0. В іншому випадку вибирається index.
 
-document.getElementById('random-image').appendChild(img);
+  // Math.min(результат_попереднього_кроку, totalSlides - 1): береться мінімальне значення між значенням, що було отримано на попередньому кроці, та totalSlides - 1. Таким чином значення index не перевищує totalSlides - 1. Якщо index більше за totalSlides - 1, то відбувається заміна на totalSlides - 1.
+
+  const transformValue = `translateX(${-currentIndex * 100}%)`;
+  document.getElementById('slider').style.transform = transformValue;
+
+  updateButtons();
+}
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+  showSlide(currentIndex - 1);
+}
+
+function updateButtons() {
+  const prevButton = document.getElementById('prevBtn');
+  const nextButton = document.getElementById('nextBtn');
+
+  prevButton.style.display = currentIndex === 0 ? 'none' : 'block';
+  nextButton.style.display =
+    currentIndex === totalSlides - 1 ? 'none' : 'block';
+}
+
+document.getElementById('prevBtn').addEventListener('click', prevSlide);
+document.getElementById('nextBtn').addEventListener('click', nextSlide);
+
+updateButtons();
