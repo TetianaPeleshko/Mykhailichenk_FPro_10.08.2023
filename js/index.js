3b1d975a908f16df2a779d749d17d5c0b4b176a7
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-20 20:25:59 +0300

    HW_22_AverageValue

--- js/index.js
+++ js/index.js
@@ -1,3 +1,28 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+// Створити скрипт який має визначити середнє арифметичне трьох чисел з насутпними умовами:
+
+// отримати від користувача через три prompt три числа
+// показати через alert середнє арифметичне цих чисел
+
+let firstNumber = prompt('Enter the first number:');
+let secondNumber = prompt('Enter the second number:');
+let thirdNumber = prompt('Enter the third number:');
+
+if (
+  firstNumber !== '' &&
+  !isNaN(firstNumber) &&
+  secondNumber !== '' &&
+  !isNaN(secondNumber) &&
+  thirdNumber !== '' &&
+  !isNaN(thirdNumber)
+) {
+  firstNumber = Number(firstNumber);
+  secondNumber = Number(secondNumber);
+  thirdNumber = Number(thirdNumber);
+
+  let resultNumber = parseFloat(
+    ((firstNumber + secondNumber + thirdNumber) / 3).toFixed(2)
+  );
+  alert(`Average value is: ${resultNumber}`);
+} else {
+  alert(`Please enter valid number`);
+}
