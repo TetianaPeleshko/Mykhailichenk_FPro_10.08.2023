fa08316d37931972247cc24952a9e0eb3fe7659c
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-22 19:19:21 +0300

    HW_24_HoursToSeconds

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+// Напишіть скрипт, який переводить години в секунди і має робити насутпне:
+
+// запитати у користувача кількість годин;
+// порахувати, скільки секунд у цій кількості годин;
+// записати обчислене значення у змінну;
+// вивести цю змінну користувачеві через alert
+
+let hours = Number(prompt('Enter the number of hours:'));
+let seconds = hours * 3600;
+
+alert(seconds);
