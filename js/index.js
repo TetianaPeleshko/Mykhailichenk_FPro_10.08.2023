177de88bd284cc328ed9a5bef9a131c0a8f11bc9
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-22 19:05:16 +0300

    HW_24_NumbersAndStrings

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+// Task 1 Отримати від користувача 3 рядки та вивести їх у довільному порядку однією командою (конкатенація);
+
+let firstStr = prompt('Enter first string:');
+let secondtStr = prompt('Enter second string:');
+let thirdStr = prompt('Enter third string:');
+
+let result = [firstStr, secondtStr, thirdStr]
+  .sort(function () {
+    return Math.random() - 0.5;
+  })
+  .join('');
+
+alert(`Concatenation result: ${result}`);
+
+// Task 2 Розбити за цифрами п'ятизначне число і вивести у вихідному порядку через пробіл
+
+let numbers = 12345;
+let digits = numbers.toString().split('').join(' ');
+alert(digits);
