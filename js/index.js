9ae29124a7da8c0e4ce3278972ae1b743a426266
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-26 01:12:18 +0300

    HW_28_generateList_ul_li

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+// Написати функцію generateList(array), яка приймає масив із чисел та масивів чисел (наприклад [1,2, 3]) і генерує список з елементів:
+
+'use strict';
+
+function generateList(array) {
+  let result = '<ul>';
+  for (let i = 0; i < array.length; i++) {
+    if (Array.isArray(array[i])) {
+      result += `<li>${generateList(array[i])}</li>`;
+    } else {
+      result += `<li>${array[i]}</li>`;
+    }
+  }
+  result += '</ul>';
+  return result;
+}
+
+const inputArray1 = [1, 2, 3];
+const inputArray2 = [1, 2, [1.1, 1.2, 1.3], 3];
+const htmlList1 = generateList(inputArray1);
+const htmlList2 = generateList(inputArray2);
+
+// Додавання списку до DOM:
+document.body.innerHTML = `${htmlList1}${htmlList2}`;
