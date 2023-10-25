5bc650dbd7e8184b49b2e1a0e765144730bdb227
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-26 00:46:33 +0300

    HW_27_Table

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+// Вивести таблицю 10 × 10, заповнену числами від 1 до 100 (таблиця створюється динамічно)
+const tableBody = document.querySelector('#tableBody');
+
+for (let i = 0; i < 10; i++) {
+  const row = document.createElement('tr');
+
+  for (let j = 0; j < 10; j++) {
+    const cell = document.createElement('td');
+    result = i * 10 + j + 1;
+    cell.textContent = result;
+    row.appendChild(cell);
+  }
+  tableBody.appendChild(row);
+}
