1899fe490aaadd93eccb4b3962fc329779523e4d
Tetiana Peleshko <t.peleshko@gmail.com>
2023-10-29 07:09:53 +0200

    HW_30_FollowTheLink

--- js/index.js
+++ js/index.js
@@ -1,3 +1,23 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+// На сторінці є дві кнопки. - переадресовується на інший сайт (за раніше введеним посиланням). Реалізувати перевірку на http/https. Якщо протокол не вказаний - додаємо
+
+function redirectToWebsite(websiteUrl) {
+  // перевіряю, чи протокол вказаний
+  if (!websiteUrl.startsWith('http://') && !websiteUrl.startsWith('https://')) {
+    // якщо true (протокол не вказаний), додаємо протокол за замовчуванням
+    websiteUrl = 'http://' + websiteUrl;
+  }
+  // пренаправлення на сайт, який був вказаний
+  window.location.href = websiteUrl;
+}
+
+document
+  .getElementById('wikipedia-button')
+  .addEventListener('click', function () {
+    redirectToWebsite('https://uk.wikipedia.org');
+  });
+
+document
+  .getElementById('ukraine_button')
+  .addEventListener('click', function () {
+    redirectToWebsite('https://uk.wikipedia.org/wiki/Україна');
+  });
