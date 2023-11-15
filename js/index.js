bf62c4b493a3b05e38a4af569d151e1710bec5f1
Tetiana Peleshko <t.peleshko@gmail.com>
2023-11-15 21:32:20 +0200

    HW_39_SuperMath

--- js/index.js
+++ js/index.js
@@ -1,3 +1,58 @@
-let userName = prompt('What is your name?', 'Your name');
-// console.log(userName);
-alert(`Hello, ${userName}! How are you?`);
+class SuperMath {
+  check(obj) {
+    if (this.isValidOperator(obj.znak)) {
+      // Виконати математичну дію, якщо оператор правильний
+      this.calculate(obj);
+    } else {
+      // Запитати нові дані через метод input()
+      this.input();
+    }
+  }
+
+  isValidOperator(operator) {
+    // Перевірити, чи є оператор допустимим
+    const validOperators = ['+', '-', '*', '/', '%'];
+    return validOperators.includes(operator);
+  }
+
+  calculate(obj) {
+    // Виконати математичну дію
+    const { X, Y, znak } = obj;
+    let result;
+
+    switch (znak) {
+      case '+':
+        result = X + Y;
+        break;
+      case '-':
+        result = X - Y;
+        break;
+      case '*':
+        result = X * Y;
+        break;
+      case '/':
+        result = X / Y;
+        break;
+      case '%':
+        result = X % Y;
+        break;
+    }
+
+    console.log(`Результат: ${result}`);
+  }
+
+  input() {
+    // Запитати нові дані від користувача
+    const X = prompt('Введіть число X:');
+    const Y = prompt('Введіть число Y:');
+    const znak = prompt('Введіть математичний оператор (+, -, *, /, %):');
+
+    const newObj = { X: parseFloat(X), Y: parseFloat(Y), znak };
+    this.check(newObj);
+  }
+}
+
+// Приклад використання:
+const p = new SuperMath();
+const obj = { X: 12, Y: 3, znak: '/' };
+p.check(obj);
