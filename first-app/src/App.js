fa61ae02514e7c0ea21a3c0f7782c9352b8c08b0
Tetiana Peleshko <t.peleshko@gmail.com>
2023-11-17 14:49:34 +0200

    HW_40_FirstReactApp

---
+++ first-app/src/App.js
@@ -0,0 +1,15 @@
+import Header from './componens/Header/Header';
+import Aside from './componens/Aside/Aside';
+import Main from './componens/Main/Main';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Aside />
+      <Main />
+    </>
+  );
+}
+
+export default App;
---
+++ first-app/src/componens/Aside/Aside.js
@@ -0,0 +1,17 @@
+import './aside.css';
+
+function Aside() {
+  return (
+    <div className="aside">
+      <h2>Side navigation</h2>
+      <ul>
+        <li>Point 1</li>
+        <li>Point 2</li>
+        <li>Point 3</li>
+        <li>Point 4</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Aside;
